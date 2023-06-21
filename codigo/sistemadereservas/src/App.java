01247935901e369ab15d009ffa02b6c867978b10
oscar27dc <134986401+oscar27dc@users.noreply.github.com>
2023-06-20 21:17:46 -0600

    parte1 proyecto
    ya les subí lo de la clase pasada nada mas den pull

--- codigo/sistemadereservas/src/App.java
+++ codigo/sistemadereservas/src/App.java
@@ -1,5 +1,9 @@
+import Bunnisestlogic.Menu;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+
+    public static void main(String[] args) throws Exception {  
+    
+        new Menu();
+        
+
